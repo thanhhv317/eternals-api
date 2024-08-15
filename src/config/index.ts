fa34f8df8fdc0dfc386034b75a249f0284c7dc97
thanhhv98 <thanhhoang317@gmail.com>
2024-08-15 20:10:58 +0700

    feat: implement jumping rope for second pets

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const SECRET_TOKEN = `Bearer ${process.env.SECRET_TOKEN}`
+export const SECOND_SECRET_TOKEN = `Bearer ${process.env.SECOND_SECRET_TOKEN}`
 export const { PORT, BOT_TOKEN } = process.env
